3259a088692b402d06c2281d9e06dd60a2b5d2b9
Hussayn Nasir <hussayn93@gmail.com>
2019-12-18 20:13:11 +0500

    Clobet with tuber

---
+++ Assets/LabBalls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabBalls : MonoBehaviour
+{
+    public float deathTime = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TurnOff();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator TurnOff()
+    {
+        yield return new WaitForSeconds(deathTime);
+        gameObject.SetActive(false);
+        yield return 0;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            HealthManager.curHealth -= 10;
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Tuber.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tuber : MonoBehaviour
+{
+
+    public GameObject projectile;
+    public GameObject tubePosition;
+
+    public GameObject throwPoint;
+    public Vector2 throwPointPosition;
+
+    public float projectileSpeed;
+
+    public bool stopShootProjectile;
+
+    public float tuberTimer;
+
+   
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stopShootProjectile = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        throwPointPosition = new Vector2(throwPoint.transform.position.x, throwPoint.transform.position.y);
+
+        if (GameManager.checkpointReached==true)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (stopShootProjectile==false)
+        {
+            StartCoroutine(ShootOnce());
+        }
+
+    }
+
+    public void CreateProjectile()
+    {
+        GameObject proj = Instantiate(projectile, tubePosition.transform.position, Quaternion.identity);
+        projectile.GetComponent<Rigidbody2D>().AddForce(throwPointPosition * projectileSpeed);
+        StopShoot();
+    }        
+            
+    public void StopShoot()
+    {
+        stopShootProjectile = true;
+    }
+    
+    private IEnumerator ShootOnce()
+    {
+        CreateProjectile();
+        yield return new WaitForSeconds(tuberTimer);
+        stopShootProjectile = false;
+
+        yield return 0;
+    }
+
+
+
+}
